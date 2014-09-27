f89477b1af0ac970267ce6c602d7fbc7f4dc430b
RAMIN Alexandre <alexandre_ramin@orange.fr>
2014-09-27 18:17:44 +0200

    update

--- Assets/SecondDisplayTexture.js
+++ Assets/SecondDisplayTexture.js
@@ -5,5 +5,5 @@ var collideBoxY : float;
 function OnMouseDown (){
 	GetComponent(SpriteRenderer).sprite = texture;
 	GetComponent(BoxCollider2D).size.x = collideBoxX;
-	GetComponent(BoxCollider2D).size.x = collideBoxY;
+	GetComponent(BoxCollider2D).size.y = collideBoxY;
 }
